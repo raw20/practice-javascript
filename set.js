7819fa94ff4eab4969bdad2a4ff715e3959c4815
정찬욱 <62588402+raw20@users.noreply.github.com>
2022-09-23 21:04:06 +0900

    set함수

---
+++ set.js
@@ -0,0 +1,25 @@
+/*
+set 함수
+중복되지 않는 유일한 값들의 집합
+요소 순서는 의미가 없음
+인덱스로 접근 불가
+ */
+
+const mySet = new Set();
+mySet.add(1);
+mySet.add(2);
+
+const obj = { a: 1, b: 2 };
+
+mySet.add(obj);
+console.log(mySet);
+console.log(mySet.has(obj)); //true
+console.log(mySet.has(3)); //false
+console.log(mySet.size);
+
+for (let item of mySet.keys()) {
+  console.log("key : ", item);
+}
+for (let [key, value] of Object.entries(obj)) {
+  console.log(`key : ${key} value : ${value}`);
+}
